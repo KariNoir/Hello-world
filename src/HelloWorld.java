657726b8861f84bbb614090f6b8098fe960d9e67
KariNoir <KariNoirm@gmail.com>
2020-01-03 14:20:30 -0500

    add both lines

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
 		System.out.println("Hello World");
+		System.out.println("Happy New Year");
+		System.out.println("Yay, 2020");
 	}
 
 }
